5844e82ff0dbb6cbe540073e4759cd6400d3340b
Maryem471 <dridimaryem95@gmail.com>
2025-01-03 14:55:11 +0100

    my first commit

---
+++ firs-react/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Hello dojo</h1>
+     <h3>Things i have to do:</h3>
+     <ol>
+      <ul>Learn react</ul>
+      <ul>climb Mr.Everst</ul>
+      <ul>run a marathon</ul>
+      <ul>feed the dogs</ul>
+     </ol>
+    </>
+  )
+}
+
+export default App
